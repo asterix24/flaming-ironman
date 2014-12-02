ed3bdb712f9c3ba89a2620cf03e073e84137595d
Gendo Ikari <gendoikari.hack@gmail.com>
2014-12-02 22:04:20 +0100

    Configuration file.

--- flame/app.py
+++ flame/app.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import yaml
 from cocos.director import director
 from root_scene import RootScene
 
 class GameApp(object):
+    DEFAULT_CFG = "./flame.cfg"
+
     def __init__(self, args):
-        director.init()
+        try:
+            cfgs = yaml.load(open(GameApp.DEFAULT_CFG, "r"))
+        except IOError:
+            cfgs = {"window": {}}
+
+        director.init(**cfgs["window"])
 
     def run(self):
         director.run(RootScene())
