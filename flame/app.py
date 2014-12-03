e2dae4775b0788d4c7518b8004e212c03de1615d
Gendo Ikari <gendoikari.hack@gmail.com>
2014-12-03 23:01:03 +0100

    Loading the test level.

--- flame/app.py
+++ flame/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import os
 import yaml
 from cocos.director import director
 from root_scene import RootScene
@@ -14,7 +15,12 @@ class GameApp(object):
         except IOError:
             cfgs = {"window": {}}
 
+        # loading the game file
+        self.game_path = cfgs["game"]["folder"]
+        game_file = os.path.join(self.game_path, cfgs["game"]["entry_file"])
+        self.game = yaml.load(open(game_file, "r"))
+
         director.init(**cfgs["window"])
 
     def run(self):
-        director.run(RootScene())
+        director.run(RootScene(self.game, self.game_path))
--- flame/root_scene.py
+++ flame/root_scene.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import os
 import cocos
 
 class RootLayer(cocos.layer.Layer):
@@ -10,6 +11,17 @@ class RootLayer(cocos.layer.Layer):
         print key
 
 class RootScene(cocos.scene.Scene):
-    def __init__(self):
+    def __init__(self, game, path):
         super(RootScene, self).__init__()
         self.add(RootLayer())
+        self.game, self.path = game, path
+
+        # load first level
+        self.loadLevel(game["levels"][0])
+
+    def loadLevel(self, level):
+        resources = cocos.tiles.load(os.path.join(self.path, level["file"]))
+
+        tilemap = resources["tiles_1"]
+        tilemap.set_view(0, 0, 1024, 768)
+        self.add(tilemap)
