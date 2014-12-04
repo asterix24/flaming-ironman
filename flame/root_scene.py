47f799f365da8a8e4dca8cba83876be99a852c58
Daniele Basile <asterix@develer.com>
2014-12-04 14:41:18 +0100

    Simple test to change layer after space key press.

--- flame/root_scene.py
+++ flame/root_scene.py
@@ -9,19 +9,32 @@ class RootLayer(cocos.layer.Layer):
 
     def on_key_press(self, key, modifiers):
         print key
+        if key == 32:
+            RootScene.instance.change_level(1)
+
 
 class RootScene(cocos.scene.Scene):
+    instance = None
     def __init__(self, game, path):
         super(RootScene, self).__init__()
+        RootScene.instance = self
         self.add(RootLayer())
         self.game, self.path = game, path
+        self.tilemap = None
 
         # load first level
-        self.loadLevel(game["levels"][1])
+        self.change_level(0)
+
+    def change_level(self, index):
+        self.loadLevel(self.game["levels"][index])
 
     def loadLevel(self, level):
         resources = cocos.tiles.load(os.path.join(self.path, level["file"]))
 
-        tilemap = resources["tiles_1"]
-        tilemap.set_view(0, 0, 1024, 768)
-        self.add(tilemap)
+        if self.tilemap is not None:
+            self.remove(self.tilemap)
+
+        self.tilemap = resources["tiles_1"]
+        self.tilemap.set_view(0, 0, 1024, 768)
+
+        self.add(self.tilemap)
