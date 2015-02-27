ac29629017c6a6a7b3939701572c635793674dc6
Daniele Basile <asterix24@gmail.com>
2015-02-27 23:23:48 +0100

    Ugly code to try cocos structure.

--- flame/root_scene.py
+++ flame/root_scene.py
@@ -4,43 +4,60 @@
 import os
 import cocos
 import main_character
+import pyglet
+from pyglet.gl import *
 
-class RootLayer(cocos.layer.Layer):
+
+class GameLayer(cocos.layer.Layer):
     is_event_handler = True
+    def __init__(self):
+        super(GameLayer, self).__init__()
+        uno = cocos.sprite.Sprite('assets/CartmanSPG.png')
+        uno.position = 300,300
+        uno.scale = 0.5
+        self.add(uno)
 
     def on_key_press(self, key, modifiers):
         print key
         # Space key code.
-        if key == 32:
-            RootScene.instance.change_level(1)
 
 
-class RootScene(cocos.scene.Scene):
-    instance = None
-    def __init__(self, game, game_path, main_character_info, main_character_path):
-        super(RootScene, self).__init__()
-        RootScene.instance = self
-        self.add(RootLayer())
+class BackgroundLayer(cocos.layer.Layer):
+    def __init__(self):
+        super(BackgroundLayer, self ).__init__()
+        self.img = pyglet.resource.image('assets/foresta.png')
+
+        uno = cocos.sprite.Sprite('assets/bat.png')
+        uno.position = 200,200
+        uno.scale = 0.5
+        self.add(uno)
+
+    def draw( self ):
+        glPushMatrix()
+        self.transform()
+        self.img.blit(0,0)
+        glPopMatrix()
 
 
-        self.game, self.game_path = game, game_path
-        self.tilemap = None
 
-        # load first level
-        self.change_level(0)
+class AnimationLayer(cocos.layer.Layer):
+    def __init__(self):
+        super(AnimationLayer, self).__init__()
 
-        self.add(main_character.MainCharacter(main_character_info, main_character_path))
-
-    def change_level(self, index):
-        self.loadLevel(self.game["levels"][index])
-
-    def loadLevel(self, level):
-        resources = cocos.tiles.load(os.path.join(self.game_path, level["file"]))
-
-        if self.tilemap is not None:
-            self.remove(self.tilemap)
-
+        resources = cocos.tiles.load('assets/level_00.tmx')
         self.tilemap = resources["tiles_1"]
         self.tilemap.set_view(0, 0, 1024, 768)
 
+
+class RootScene(cocos.scene.Scene):
+    def __init__(self, game, game_path, main_character_info, main_character_path):
+        super(RootScene, self).__init__()
+
+        resources = cocos.tiles.load('assets/level_00.tmx')
+        self.tilemap = resources["tiles_1"]
+        self.tilemap.set_view(0, 0, 1024, 768)
+
+        self.add(BackgroundLayer())
         self.add(self.tilemap)
+        self.add(GameLayer())
+
